07b6d128ca80aa4a9d8354cd3441eef2712cd7d3
Felipe Alves <felipe.alves.cs@hotmail.com>
2018-11-27 22:02:17 -0200

    Adicionado Script para criar as tabelas no SQL Lite

---
+++ FelipeCore.Clientes/Create Tables.sql
@@ -0,0 +1,28 @@
+﻿PRAGMA foreign_keys = off;
+
+BEGIN TRANSACTION;
+
+CREATE TABLE Clientes (
+    Id       INTEGER        PRIMARY KEY AUTOINCREMENT
+                            NOT NULL,
+    Nome     VARCHAR (2000),
+    Email    VARCHAR (200),
+    Telefone VARCHAR (20) 
+);
+
+INSERT INTO Clientes (
+                         Id,
+                         Nome,
+                         Email,
+                         Telefone
+                     )
+                     VALUES (
+                         1,
+                         'Felipe3',
+                         'felipe603@gmail.com',
+                         '1111'
+                     );
+
+COMMIT TRANSACTION ; 
+
+PRAGMA foreign_keys = on;
